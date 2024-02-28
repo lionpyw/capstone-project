9e2aceef0241720de44fb98a379c0283403f3ee6
lionpyw <lionpaaji@gmail.com>
2024-02-28 14:44:33 +0500

    second commit

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,35 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 from . import models
-# Register your models here.
 
-admin.site.register(models.User)
+admin.site.site_header = "Consultation Network Admin"
+admin.site.index_title = "Admin"
+
+@admin.register(models.User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "is_service_provider",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2', 'email',
+                        'first_name', 'last_name',"is_service_provider"),
+        }),
+    )
